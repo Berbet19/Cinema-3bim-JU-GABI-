6a484d1652404dd557a770ce53ec1b5e5a6d66f4
Berbet <juliatrindadeberbet@gmail.com>
2026-09-09 19:07:40 -0300

    :construction: Design no header + arquivo movies.ts

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,20 +8,30 @@ import CadastroScreen from './cadastro';
 import SeriesScreen from './series';
 
 
+
 const Tab = createMaterialTopTabNavigator();
 
 function MeusTabs() {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: '#6200ee',
-        tabBarInactiveTintColor: '#757575',
-        tabBarIndicatorStyle: { backgroundColor: '#6200ee' },
+        tabBarActiveTintColor: '#fff8f8',
+        tabBarInactiveTintColor: '#f5f5f5',
+        tabBarIndicatorStyle: { backgroundColor: '#7a0505' },
+      tabBarStyle:{
+        backgroundColor: '#7a0505',
+         elevation: 0,       
+      shadowOpacity: 0,     
+      borderBottomWidth: 0,    
+      borderTopWidth: 0,    
+      }
+     
+    
       }}
     >
-       <Tab.Screen name="index" component={IndexScreen} options={{ title: 'home' }} />
+       <Tab.Screen name="index" component={IndexScreen} options={{ title: 'Home' }} />
        <Tab.Screen name="series" component={SeriesScreen} options={{ title: 'Séries' }} />
-      <Tab.Screen name="cadastro" component={CadastroScreen} options={{ title: 'cadastro' }} />
+      <Tab.Screen name="cadastro" component={CadastroScreen} options={{ title: 'Cadastro' }} />
       
      
 
@@ -42,20 +52,25 @@ export default function App() {
           component={MeusTabs} 
           options={{ 
             title: 'CINEMANDO', // Título do Header principal
+            headerShadowVisible: false, // Remove a linha/sombra no iOS e Android
+            headerTitleAlign: 'center', 
+    
             headerStyle: { 
-              backgroundColor: '#6200ee',
+              backgroundColor: '#000000',
               elevation: 0, // Remove a sombra no Android para fundir com as abas
               shadowOpacity: 0, // Remove a sombra no iOS
-
             },
             headerTitleStyle:{
                 alignItems:'center',
                 justifyContent: 'center',
-                letterSpacing: 5,
+                letterSpacing: 7,
+                fontSize: 69,
+                fontWeight: 800,
+                fontFamily:   'chewy system-ui',
                 
 
             },
-            headerTintColor: '#fff',
+            headerTintColor: '#920606',
           }} 
         />
       </Stack.Navigator>
@@ -68,6 +83,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#8b0101',
   },
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function IndexScreen() {
   return (
-    <View >
-      <Text>OIE</Text>
+    <View style={styles.center}>
+        <View>
+            
+        </View>
+      
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+});
